7cbc1f78248bde7469d6051310c490c89d454f1b
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-06-06 12:12:36 +0400

    test app created

---
+++ TestApplication/src/testapplication/Main.java
@@ -0,0 +1,20 @@
+package testapplication;
+
+/**
+ * Test application main class.
+ *
+ * @version 1.01, 04 June 2013
+ * @since 04 June 2013
+ * @author Ilya Gubarev
+ */
+public abstract class Main {
+
+    /**
+     * Application entry point.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
